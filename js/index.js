7299c1cc721ce9469735f2f12222f5b8ceda5d70
Maksim <erominmaksim@gmail.com>
2022-04-27 15:58:59 +0300

    Update index.js
    temporarily removed, while it is not on the backend

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-$(document).ready(function() {
-  $("#submit").click(function() {
-      let name = $("#name").val();
-      let email = $("#email").val();
-      let number = $("#number").val();
+// $(document).ready(function() {
+//   $("#submit").click(function() {
+//       let name = $("#name").val();
+//       let email = $("#email").val();
+//       let number = $("#number").val();
 
-      if (name.length == "") {
-          $(".error1").text("Это поле обязательно");
-          return false;
-      } 
-      else if (email.length == "") {
-          $(".error2").text("Это поле обязательно");
-          return false;
-      }
-      else if (!email.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)) {
-          $(".error2").text("Неправельный формат почты!");
-          return false;
-      }
-      else if (number.length == "") {
-          $(".error3").text("Это поле обязательно");
-          return false;
-      }
-      else if (!number.match(/([0-9])\w+/)) {
-          $(".error3").text("Цыфровой ввод");
-          return false;
-      } else {
-          return true;
-      }
-  }) 
-});
+//       if (name.length == "") {
+//           $(".error1").text("Это поле обязательно");
+//           return false;
+//       } 
+//       else if (email.length == "") {
+//           $(".error2").text("Это поле обязательно");
+//           return false;
+//       }
+//       else if (!email.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)) {
+//           $(".error2").text("Неправельный формат почты!");
+//           return false;
+//       }
+//       else if (number.length == "") {
+//           $(".error3").text("Это поле обязательно");
+//           return false;
+//       }
+//       else if (!number.match(/([0-9])\w+/)) {
+//           $(".error3").text("Цыфровой ввод");
+//           return false;
+//       } else {
+//           return true;
+//       }
+//   }) 
+// }); for django
